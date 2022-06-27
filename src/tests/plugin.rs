e80ee29df89c9ea9cbcfd903659f5f1b3e683b04
Robbert van der Helm <mail@robbertvanderhelm.nl>
2022-06-27 16:28:42 +0200

    Rename the processing test case
    Since this one does both audio and MIDI. We should have a similar basic
    test case for just audio or just MIDI.

--- src/tests/plugin.rs
+++ src/tests/plugin.rs
@@ -11,15 +11,16 @@ use crate::plugin::ext::audio_ports::{AudioPortConfig, AudioPorts};
 use crate::plugin::ext::note_ports::{NotePort, NotePortConfig, NotePorts};
 use crate::plugin::library::PluginLibrary;
 
-/// The string representation for [`PluginTestCase::BasicAudioProcessing`].
-const BASIC_AUDIO_PROCESSING: &str = "process-basic";
+/// The string representation for [`PluginTestCase::BasicCombinedAudioProcessing`].
+const BASIC_COMBINED_AUDIO_PROCESSING: &str = "process-combined-basic";
 
 /// The tests for individual CLAP plugins. See the module's heading for more information.
 pub enum PluginTestCase {
     /// Sends audio and MIDI to the plugin (depending on what it supports) with the initial
     /// parmaeters, and asserts that the audio output does not contain any non-finite or subnormal
     /// values.
-    BasicAudioProcessing,
+    // TODO: Similar test cases that only do audio or only do MIDI
+    BasicCombinedAudioProcessing,
 }
 
 impl<'a> TestCase<'a> for PluginTestCase {
@@ -27,24 +28,24 @@ impl<'a> TestCase<'a> for PluginTestCase {
     /// should be tested.
     type TestArgs = (&'a PluginLibrary, &'a str);
 
-    const ALL: &'static [Self] = &[PluginTestCase::BasicAudioProcessing];
+    const ALL: &'static [Self] = &[PluginTestCase::BasicCombinedAudioProcessing];
 
     fn from_str(string: &str) -> Option<Self> {
         match string {
-            BASIC_AUDIO_PROCESSING => Some(PluginTestCase::BasicAudioProcessing),
+            BASIC_COMBINED_AUDIO_PROCESSING => Some(PluginTestCase::BasicCombinedAudioProcessing),
             _ => None,
         }
     }
 
     fn as_str(&self) -> &'static str {
         match &self {
-            PluginTestCase::BasicAudioProcessing => BASIC_AUDIO_PROCESSING,
+            PluginTestCase::BasicCombinedAudioProcessing => BASIC_COMBINED_AUDIO_PROCESSING,
         }
     }
 
     fn description(&self) -> String {
         match &self {
-            PluginTestCase::BasicAudioProcessing => String::from("Sends random audio and/or MIDI through the plugin with its default parameter values and tests whether the output does not contain any non-finite or subnormal values."),
+            PluginTestCase::BasicCombinedAudioProcessing => String::from("Sends random audio and/or MIDI through the plugin with its default parameter values and tests whether the output does not contain any non-finite or subnormal values."),
         }
     }
 
@@ -65,7 +66,7 @@ impl<'a> TestCase<'a> for PluginTestCase {
 
     fn run_in_process(&self, (library, plugin_id): Self::TestArgs) -> TestResult {
         let result = match &self {
-            PluginTestCase::BasicAudioProcessing => {
+            PluginTestCase::BasicCombinedAudioProcessing => {
                 // The host doesn't need to do anything special for this test
                 let host = ClapHost::new();
                 let process_result = library
