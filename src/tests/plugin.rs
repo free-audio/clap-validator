0816ee8cd458798947a6f4bb9e2d7db9f457240d
Robbert van der Helm <mail@robbertvanderhelm.nl>
2022-07-02 00:25:26 +0200

    Test parameter conversion consistency

--- src/tests/plugin.rs
+++ src/tests/plugin.rs
@@ -2,6 +2,7 @@
 
 use anyhow::Context;
 use clap::ValueEnum;
+use rand::Rng;
 use std::process::Command;
 
 use self::processing::ProcessingTest;
@@ -11,6 +12,7 @@ use crate::hosting::ClapHost;
 use crate::plugin::audio_thread::process::ProcessConfig;
 use crate::plugin::ext::audio_ports::{AudioPortConfig, AudioPorts};
 use crate::plugin::ext::note_ports::NotePorts;
+use crate::plugin::ext::params::Params;
 use crate::plugin::library::PluginLibrary;
 use crate::tests::rng::{new_prng, NoteGenerator};
 
@@ -19,6 +21,7 @@ mod processing;
 const BASIC_OUT_OF_PLACE_AUDIO_PROCESSING: &str = "process-audio-out-of-place-basic";
 const BASIC_OUT_OF_PLACE_NOTE_PROCESSING: &str = "process-note-out-of-place-basic";
 const INCONSISTENT_NOTE_PROCESSING: &str = "process-note-inconsistent";
+const CONVERT_PARAMS: &str = "param-conversions";
 
 /// The tests for individual CLAP plugins. See the module's heading for more information, and the
 /// `description` function below for a description of each test case.
@@ -26,6 +29,7 @@ pub enum PluginTestCase {
     BasicOutOfPlaceAudioProcessing,
     BasicOutOfPlaceNoteProcessing,
     InconsistentNoteProcessing,
+    ConvertParams,
 }
 
 impl<'a> TestCase<'a> for PluginTestCase {
@@ -37,6 +41,7 @@ impl<'a> TestCase<'a> for PluginTestCase {
         PluginTestCase::BasicOutOfPlaceAudioProcessing,
         PluginTestCase::BasicOutOfPlaceNoteProcessing,
         PluginTestCase::InconsistentNoteProcessing,
+        PluginTestCase::ConvertParams,
     ];
 
     fn from_str(string: &str) -> Option<Self> {
@@ -48,6 +53,7 @@ impl<'a> TestCase<'a> for PluginTestCase {
                 Some(PluginTestCase::BasicOutOfPlaceNoteProcessing)
             }
             INCONSISTENT_NOTE_PROCESSING => Some(PluginTestCase::InconsistentNoteProcessing),
+            CONVERT_PARAMS => Some(PluginTestCase::ConvertParams),
             _ => None,
         }
     }
@@ -57,6 +63,7 @@ impl<'a> TestCase<'a> for PluginTestCase {
             PluginTestCase::BasicOutOfPlaceAudioProcessing => BASIC_OUT_OF_PLACE_AUDIO_PROCESSING,
             PluginTestCase::BasicOutOfPlaceNoteProcessing => BASIC_OUT_OF_PLACE_NOTE_PROCESSING,
             PluginTestCase::InconsistentNoteProcessing => INCONSISTENT_NOTE_PROCESSING,
+            PluginTestCase::ConvertParams => CONVERT_PARAMS,
         }
     }
 
@@ -65,6 +72,7 @@ impl<'a> TestCase<'a> for PluginTestCase {
             PluginTestCase::BasicOutOfPlaceAudioProcessing => String::from("Processes random audio through the plugin with its default parameter values and tests whether the output does not contain any non-finite or subnormal values. Uses out-of-place audio processing."),
             PluginTestCase::BasicOutOfPlaceNoteProcessing => String::from("Sends audio and random note and MIDI events to the plugin with its default parameter values and tests the output for consistency. Uses out-of-place audio processing."),
             PluginTestCase::InconsistentNoteProcessing => String::from("Sends intentionally inconsistent and mismatching note and MIDI events to the plugin with its default parameter values and tests the output for consistency. Uses out-of-place audio processing."),
+            PluginTestCase::ConvertParams => String::from("Asserts that value to string and string to value conversions are support for all end/or none of the plugin's parameters, and that conversions between values and strings roundtrip correctly."),
         }
     }
 
@@ -297,6 +305,107 @@ impl<'a> TestCase<'a> for PluginTestCase {
                             },
                         )?;
 
+
+                        host.thread_safety_check()
+                            .context("Thread safety checks failed")?;
+
+                        Ok(TestStatus::Success { notes: None })
+                    });
+
+                match result {
+                    Ok(status) => status,
+                    Err(err) => TestStatus::Failed {
+                        reason: Some(format!("{err:#}")),
+                    },
+                }
+            }
+            PluginTestCase::ConvertParams => {
+                let mut prng = new_prng();
+
+                let host = ClapHost::new();
+                let result = library
+                    .create_plugin(plugin_id, host.clone())
+                    .context("Could not create the plugin instance")
+                    .and_then(|plugin| {
+                        plugin.init().context("Error during initialization")?;
+
+                        let params = match plugin.get_extension::<Params>() {
+                            Some(params) => params,
+                            None => {
+                                return Ok(TestStatus::Skipped {
+                                    reason: Some(String::from(
+                                        "The plugin does not support the 'params' extension.",
+                                    )),
+                                })
+                            }
+                        };
+
+                        let param_infos = params
+                            .info()
+                            .context("Failure while fetching the plugin's parameters")?;
+                        let num_params = param_infos.len();
+
+                        // We keep track of how many parameters support these conversions. A plugin
+                        // should support either conversion either for all of its parameters, or for
+                        // none of them.
+                        const VALUES_PER_PARAM: usize = 6;
+                        let expected_conversions = param_infos.len() * VALUES_PER_PARAM;
+
+                        let mut num_supported_value_to_text = 0;
+                        let mut num_supported_text_to_value = 0;
+                        'param_loop: for (param_id, param_info) in param_infos {
+                            // For each parameter we'll test this for the minimum and maximum values
+                            // (in case these values have special meanings), and four other random
+                            // values
+                            let values: [f64; VALUES_PER_PARAM] = [
+                                *param_info.range.start(),
+                                *param_info.range.end(),
+                                prng.gen_range(param_info.range.clone()),
+                                prng.gen_range(param_info.range.clone()),
+                                prng.gen_range(param_info.range.clone()),
+                                prng.gen_range(param_info.range),
+                            ];
+                            for starting_value in values {
+                                // If the plugin rounds string representations then `value` may very
+                                // will not roundtrip correctly, so we'll start at the string
+                                // representation
+                                let starting_text =
+                                    match params.value_to_text(param_id, starting_value) {
+                                        Ok(text) => text,
+                                        Err(_) => continue 'param_loop,
+                                    };
+                                num_supported_value_to_text += 1;
+                                let reconverted_value =
+                                    match params.text_to_value(param_id, &starting_text) {
+                                        Ok(value) => value,
+                                        Err(_) => continue 'param_loop,
+                                    };
+                                num_supported_text_to_value += 1;
+
+                                let reconverted_text =
+                                    params.value_to_text(param_id, reconverted_value)?;
+                                // Both of these are produced by the plugin, so they should be equal
+                                if starting_text != reconverted_text {
+                                    anyhow::bail!("Converting {starting_value} to a string, back to a value, and then back to a string again results in '{starting_text}' -> {reconverted_value} -> '{reconverted_text}', which is not consistent.");
+                                }
+
+                                // And one last hop back for good measure
+                                let final_value =
+                                    params.text_to_value(param_id, &reconverted_text)?;
+                                if final_value != reconverted_value {
+                                    anyhow::bail!("Converting {starting_value} to a string, back to a value, back to a string, and then back to a value again results in '{starting_text}' -> {reconverted_value} -> '{reconverted_text}' -> {final_value}, which is not consistent.");
+                                }
+                            }
+                        }
+
+                        if num_supported_value_to_text != 0 && num_supported_value_to_text != expected_conversions {
+                            let num_supported_params = num_supported_value_to_text / VALUES_PER_PARAM;
+                            anyhow::bail!("'clap_plugin_params::value_to_text()' is supported for {num_supported_params} out of {num_params} parameters. This function is expected to be supported for either none of the parameters or for all of them.");
+                        }
+                        if num_supported_text_to_value != 0 && num_supported_text_to_value != expected_conversions {
+                            let num_supported_params = num_supported_text_to_value / VALUES_PER_PARAM;
+                            anyhow::bail!("'clap_plugin_params::text_to_value()' is supported for {num_supported_params} out of {num_params} parameters. This function is expected to be supported for either none of the parameters or for all of them.");
+                        }
 
                         host.thread_safety_check()
                             .context("Thread safety checks failed")?;
