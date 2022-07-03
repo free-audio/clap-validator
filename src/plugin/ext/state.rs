664853678e8d58da3703088aeac4f3424919fc2b
Robbert van der Helm <mail@robbertvanderhelm.nl>
2022-07-03 15:52:31 +0200

    Add abstractions for buffered state saves/loads

--- src/plugin/ext/state.rs
+++ src/plugin/ext/state.rs
@@ -31,6 +31,9 @@ struct InputStream<'a> {
     /// provides the buffer we should copy data into, and subsequent reads should continue from
     /// where we were left off.
     read_position: AtomicUsize,
+    /// The maximum number of bytes this stream will return at a time, if the stream pretends to be
+    /// buffered. This is used to test whether the plugin handles buffered streams correctly.
+    max_read_size: Option<usize>,
 }
 
 /// An output stream backed by a vector.
@@ -43,6 +46,10 @@ struct OutputStream {
     // no reason for the plugin to be calling the stream read and write methods from multiple
     // threads, but better be safe than sorry.
     buffer: Mutex<Vec<u8>>,
+    /// The maximum number of bytes the plugin is allowed to write to this stream at a time, if the
+    /// stream pretends to be buffered. This is used to test whether the plugin handles buffered
+    /// streams correctly.
+    max_write_size: Option<usize>,
 }
 
 impl<'a> Extension<&'a Plugin<'a>> for State<'a> {
@@ -70,6 +77,21 @@ impl State<'_> {
         }
     }
 
+    /// Retrieve the plugin's state while limiting the number of bytes the plugin can write at a
+    /// time. Returns an error if the plugin returned `false`.
+    pub fn save_buffered(&self, max_bytes: usize) -> Result<Vec<u8>> {
+        let stream = OutputStream::new().with_buffering(max_bytes);
+
+        if unsafe { (self.state.as_ref().save)(self.plugin.as_ptr(), &stream.vtable) } {
+            Ok(stream.into_vec())
+        } else {
+            anyhow::bail!(
+                "'clap_plugin_state::save()' returned false when only allowing {max_bytes} bytes \
+                 to be written at a time."
+            );
+        }
+    }
+
     /// Restore previously stored state. Returns an error if the plugin returned `false`.
     pub fn load(&self, state: &[u8]) -> Result<()> {
         let stream = InputStream::new(state);
@@ -78,6 +100,21 @@ impl State<'_> {
             Ok(())
         } else {
             anyhow::bail!("'clap_plugin_state::load()' returned false.");
+        }
+    }
+
+    /// Restore previously stored state while limiting the number of bytes the plugin can read at a
+    /// time. Returns an error if the plugin returned `false`.
+    pub fn load_buffered(&self, state: &[u8], max_bytes: usize) -> Result<()> {
+        let stream = InputStream::new(state).with_buffering(max_bytes);
+
+        if unsafe { (self.state.as_ref().load)(self.plugin.as_ptr(), &stream.vtable) } {
+            Ok(())
+        } else {
+            anyhow::bail!(
+                "'clap_plugin_state::load()' returned false when only allowing {max_bytes} bytes \
+                 to be read at a time."
+            );
         }
     }
 }
@@ -93,16 +130,26 @@ impl<'a> InputStream<'a> {
 
             buffer,
             read_position: AtomicUsize::new(0),
+            max_read_size: None,
         })
+    }
+
+    /// Only allow `max_bytes` bytes to be read at a time. Useful for simulating buffered streams.
+    pub fn with_buffering(mut self: Pin<Box<Self>>, max_bytes: usize) -> Pin<Box<Self>> {
+        self.max_read_size = Some(max_bytes);
+        self
     }
 
     unsafe extern "C" fn read(stream: *const clap_istream, buffer: *mut c_void, size: u64) -> i64 {
         check_null_ptr!(0, stream, buffer);
         let this = &*(stream as *const Self);
 
-        // TODO: Add a way to configure this stream to limit reads and writes to a specific size.
-        //       We can use this to assert that the plugin can recover from this, because it's
-        //       technically allowed.
+        // The writes may be limited to a certain buffering size to test the plugin's capabilities
+        let size = match this.max_read_size {
+            Some(max_read_size) => size.min(max_read_size as u64),
+            None => size,
+        };
+
         let current_pos = this.read_position.load(Ordering::Relaxed);
         let bytes_to_read = (this.buffer.len() - current_pos).min(size as usize);
         this.read_position
@@ -125,7 +172,15 @@ impl OutputStream {
             },
 
             buffer: Mutex::new(Vec::new()),
+            max_write_size: None,
         })
+    }
+
+    /// Only allow `max_bytes` bytes to be written at a time. Useful for simulating buffered
+    /// streams.
+    pub fn with_buffering(mut self: Pin<Box<Self>>, max_bytes: usize) -> Pin<Box<Self>> {
+        self.max_write_size = Some(max_bytes);
+        self
     }
 
     /// Get the byte buffer from this stream.
@@ -145,7 +200,12 @@ impl OutputStream {
         check_null_ptr!(0, stream, buffer);
         let this = &*(stream as *const Self);
 
-        // TODO: The same as in the InputStream
+        // The writes may be limited to a certain buffering size to test the plugin's capabilities
+        let size = match this.max_write_size {
+            Some(max_write_size) => size.min(max_write_size as u64),
+            None => size,
+        };
+
         this.buffer
             .lock()
             .unwrap()
