9079d07418ba2153c9c9441716f3260aa040229d
Robbert van der Helm <mail@robbertvanderhelm.nl>
2022-11-04 12:57:42 +0100

    Move plugin feature retrieval to a function

--- src/tests/plugin/features.rs
+++ src/tests/plugin/features.rs
@@ -1,6 +1,6 @@
 //! Tests surrounding plugin features.
 
-use anyhow::Context;
+use anyhow::{Context, Result};
 use clap_sys::plugin_features::{
     CLAP_PLUGIN_FEATURE_ANALYZER, CLAP_PLUGIN_FEATURE_AUDIO_EFFECT, CLAP_PLUGIN_FEATURE_INSTRUMENT,
     CLAP_PLUGIN_FEATURE_NOTE_EFFECT,
@@ -12,46 +12,31 @@ use crate::tests::TestStatus;
 /// Check whether the plugin's categories are consistent. Currently this just makes sure that the
 /// plugin has one of the four main plugin category features.
 pub fn test_category_features(library: &PluginLibrary, plugin_id: &str) -> TestStatus {
-    let result = library
-        .metadata()
-        .with_context(|| {
-            format!(
-                "Could not fetch plugin metadata for '{}'",
-                library.library_path().display()
-            )
-        })
-        .map(|metadata| {
-            metadata
-                .plugins
-                .into_iter()
-                .find(|plugin_meta| plugin_meta.id == plugin_id)
-                .expect("Incorrect plugin ID for metadata query, this is a bug in clap-validator")
-        })
-        .and_then(|metadata| {
-            // These are stored in the bindings as C-compatible null terminated strings, but we'll
-            // need them as regular string slices so we can compare them to
-            let instrument_feature = CLAP_PLUGIN_FEATURE_INSTRUMENT.to_str().unwrap();
-            let audio_effect_feature = CLAP_PLUGIN_FEATURE_AUDIO_EFFECT.to_str().unwrap();
-            let note_effect_feature = CLAP_PLUGIN_FEATURE_NOTE_EFFECT.to_str().unwrap();
-            let analyzer_feature = CLAP_PLUGIN_FEATURE_ANALYZER.to_str().unwrap();
+    let result = plugin_features(library, plugin_id).and_then(|features| {
+        // These are stored in the bindings as C-compatible null terminated strings, but we'll
+        // need them as regular string slices so we can compare them to
+        let instrument_feature = CLAP_PLUGIN_FEATURE_INSTRUMENT.to_str().unwrap();
+        let audio_effect_feature = CLAP_PLUGIN_FEATURE_AUDIO_EFFECT.to_str().unwrap();
+        let note_effect_feature = CLAP_PLUGIN_FEATURE_NOTE_EFFECT.to_str().unwrap();
+        let analyzer_feature = CLAP_PLUGIN_FEATURE_ANALYZER.to_str().unwrap();
 
-            let has_main_category = metadata.features.iter().any(|feature| -> bool {
-                feature == instrument_feature
-                    || feature == audio_effect_feature
-                    || feature == note_effect_feature
-                    || feature == analyzer_feature
-            });
-
-            if has_main_category {
-                Ok(TestStatus::Success { details: None })
-            } else {
-                anyhow::bail!(
-                    "The plugin needs to have at least one of thw following plugin category \
-                     features: \"{instrument_feature}\", \"{audio_effect_feature}\", \
-                     \"{note_effect_feature}\", or \"{analyzer_feature}\""
-                )
-            }
+        let has_main_category = features.iter().any(|feature| -> bool {
+            feature == instrument_feature
+                || feature == audio_effect_feature
+                || feature == note_effect_feature
+                || feature == analyzer_feature
         });
+
+        if has_main_category {
+            Ok(TestStatus::Success { details: None })
+        } else {
+            anyhow::bail!(
+                "The plugin needs to have at least one of thw following plugin category features: \
+                 \"{instrument_feature}\", \"{audio_effect_feature}\", \"{note_effect_feature}\", \
+                 or \"{analyzer_feature}\""
+            )
+        }
+    });
 
     match result {
         Ok(status) => status,
@@ -59,4 +44,25 @@ pub fn test_category_features(library: &PluginLibrary, plugin_id: &str) -> TestS
             details: Some(format!("{err:#}")),
         },
     }
+}
+
+/// Get the feature vector for a plugin in the library. Returns `None` if the plugin ID does not
+/// exist in the library.
+fn plugin_features(library: &PluginLibrary, plugin_id: &str) -> Result<Vec<String>> {
+    library
+        .metadata()
+        .with_context(|| {
+            format!(
+                "Could not fetch plugin metadata for '{}'",
+                library.library_path().display()
+            )
+        })
+        .and_then(|metadata| {
+            metadata
+                .plugins
+                .into_iter()
+                .find(|plugin_meta| plugin_meta.id == plugin_id)
+                .context("Incorrect plugin ID for metadata query, this is a bug in clap-validator")
+        })
+        .map(|metadata| metadata.features)
 }
