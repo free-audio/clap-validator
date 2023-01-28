4d03abd3971ef4daa0b6524f3590b5115158d408
Robbert van der Helm <mail@robbertvanderhelm.nl>
2023-01-29 00:06:43 +0100

    Write parameter values to file when fuzzing fails

--- src/tests/plugin/params.rs
+++ src/tests/plugin/params.rs
@@ -4,9 +4,11 @@ use anyhow::{Context, Result};
 use clap_sys::events::CLAP_EVENT_PARAM_VALUE;
 use clap_sys::id::clap_id;
 use rand::Rng;
+use serde::Serialize;
 use std::collections::BTreeMap;
 
 use super::processing::ProcessingTest;
+use super::PluginTestCase;
 use crate::host::Host;
 use crate::plugin::ext::audio_ports::{AudioPortConfig, AudioPorts};
 use crate::plugin::ext::note_ports::NotePorts;
@@ -14,7 +16,7 @@ use crate::plugin::ext::params::Params;
 use crate::plugin::instance::process::{Event, ProcessConfig};
 use crate::plugin::library::PluginLibrary;
 use crate::tests::rng::{new_prng, NoteGenerator, ParamFuzzer};
-use crate::tests::TestStatus;
+use crate::tests::{TestCase, TestStatus};
 
 /// The fixed buffer size to use for these tests.
 const BUFFER_SIZE: usize = 512;
@@ -23,6 +25,20 @@ pub const FUZZ_NUM_PERMUTATIONS: usize = 50;
 /// How many buffers of [`BUFFER_SIZE`] samples to process at each parameter permutation. This
 /// allows the plugin's state to settle in before moving to the next set of parameter values.
 pub const FUZZ_RUNS_PER_PERMUTATION: usize = 5;
+
+/// The file name we'll use to dump the previous parameter values when a fuzzing test fails.
+const PREVIOUS_PARAM_VALUES_FILE_NAME: &str = "param-values-previous.json";
+/// The file name we'll use to dump the current parameter values when a fuzzing test fails.
+const CURRENT_PARAM_VALUES_FILE_NAME: &str = "param-values-current.json";
+
+/// The format parameter values will be written in when the fuzzing test fails. Used only for
+/// serialization.
+#[derive(Debug, Serialize)]
+struct ParamValue<'a> {
+    id: clap_id,
+    name: &'a str,
+    value: f64,
+}
 
 /// The test for `ProcessingTest::ConvertParams`.
 pub fn test_convert_params(library: &PluginLibrary, plugin_id: &str) -> Result<TestStatus> {
@@ -213,46 +229,93 @@ pub fn test_random_fuzz_params(library: &PluginLibrary, plugin_id: &str) -> Resu
     let param_fuzzer = ParamFuzzer::new(&param_infos);
     let mut note_event_rng = note_ports_config.map(NoteGenerator::new);
 
+    // We'll keep track of the current and the previous set of parameter value so we can write them
+    // to a file if the test fails
+    let mut current_events: Option<Vec<Event>>;
+    let mut previous_events: Option<Vec<Event>> = None;
+
     let (mut input_buffers, mut output_buffers) = audio_ports_config
         .unwrap_or_default()
         .create_buffers(BUFFER_SIZE);
     for permutation_no in 1..=FUZZ_NUM_PERMUTATIONS {
-        // These are taken out of the `Option` and set during the first run
-        let mut random_param_set_events: Option<Vec<_>> =
-            Some(param_fuzzer.randomize_params_at(&mut prng, 0).collect());
+        current_events = Some(param_fuzzer.randomize_params_at(&mut prng, 0).collect());
 
-        ProcessingTest::new_out_of_place(&plugin, &mut input_buffers, &mut output_buffers)?
-            .run(
-                FUZZ_RUNS_PER_PERMUTATION,
-                ProcessConfig::default(),
-                |process_data| {
-                    if let Some(random_param_set_events) = random_param_set_events.take() {
-                        *process_data.input_events.events.lock() = random_param_set_events;
-                    }
+        let mut have_set_parameters = false;
+        let run_result =
+            ProcessingTest::new_out_of_place(&plugin, &mut input_buffers, &mut output_buffers)?
+                .run(
+                    FUZZ_RUNS_PER_PERMUTATION,
+                    ProcessConfig::default(),
+                    |process_data| {
+                        if !have_set_parameters {
+                            *process_data.input_events.events.lock() =
+                                current_events.clone().unwrap();
+                            have_set_parameters = true;
+                        }
 
-                    // Audio and MIDI/note events are randomized in accordance to what the plugin
-                    // supports
-                    if let Some(note_event_rng) = note_event_rng.as_mut() {
-                        // This includes a sort if `random_param_set_events` also contained a queue
-                        note_event_rng.fill_event_queue(
-                            &mut prng,
-                            &process_data.input_events,
-                            BUFFER_SIZE as u32,
-                        )?;
-                    }
-                    process_data.buffers.randomize(&mut prng);
+                        // Audio and MIDI/note events are randomized in accordance to what the plugin
+                        // supports
+                        if let Some(note_event_rng) = note_event_rng.as_mut() {
+                            // This includes a sort if `random_param_set_events` also contained a queue
+                            note_event_rng.fill_event_queue(
+                                &mut prng,
+                                &process_data.input_events,
+                                BUFFER_SIZE as u32,
+                            )?;
+                        }
+                        process_data.buffers.randomize(&mut prng);
 
-                    Ok(())
-                },
-            )
-            .with_context(|| {
-                // TODO: Write the current and previous values of `random_param_set_events` to a
-                //       file if processing failed
-                format!(
-                    "Invalid output detected in parameter value permutation {permutation_no} of \
-                     {FUZZ_NUM_PERMUTATIONS}"
-                )
-            })?;
+                        Ok(())
+                    },
+                );
+
+        // If the run failed we'll want to write the parameter values to a file first
+        if run_result.is_err() {
+            let (previous_param_values_file_path, previous_param_values_file) =
+                PluginTestCase::RandomFuzzParams
+                    .temporary_file(plugin_id, PREVIOUS_PARAM_VALUES_FILE_NAME)?;
+            let (current_param_values_file_path, current_param_values_file) =
+                PluginTestCase::RandomFuzzParams
+                    .temporary_file(plugin_id, CURRENT_PARAM_VALUES_FILE_NAME)?;
+
+            let create_param_values_vec = |events: Option<Vec<Event>>| match events {
+                Some(events) => events
+                    .into_iter()
+                    .map(|event| match event {
+                        Event::ParamValue(event) => ParamValue {
+                            id: event.param_id,
+                            name: &param_infos[&event.param_id].name,
+                            value: event.value,
+                        },
+                        _ => panic!("Unexpected event type. This is a clap-validator bug."),
+                    })
+                    .collect(),
+                None => Vec::new(),
+            };
+            let previous_param_values: Vec<ParamValue> = create_param_values_vec(previous_events);
+            let current_param_values: Vec<ParamValue> = create_param_values_vec(current_events);
+
+            serde_json::to_writer_pretty(previous_param_values_file, &previous_param_values)?;
+            serde_json::to_writer_pretty(current_param_values_file, &current_param_values)?;
+
+            // This is a bit weird and there may be a better way to do this, but we only want to
+            // write the parameter values if we know the run has failed, and we only know the
+            // filename after writing those values to a file
+            return Err(run_result
+                .with_context(|| {
+                    format!(
+                        "Invalid output detected in parameter value permutation {} of {} ('{}' \
+                         and '{}' contain the current and previous parameter values)",
+                        permutation_no,
+                        FUZZ_NUM_PERMUTATIONS,
+                        current_param_values_file_path.display(),
+                        previous_param_values_file_path.display(),
+                    )
+                })
+                .unwrap_err());
+        }
+
+        std::mem::swap(&mut previous_events, &mut current_events);
     }
 
     // `ProcessingTest::run()` already handled callbacks for us
