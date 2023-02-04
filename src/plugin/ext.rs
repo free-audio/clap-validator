e1db2417dc2572d3a9a88cff3ec85845f68ad777
Robbert van der Helm <mail@robbertvanderhelm.nl>
2023-02-04 19:29:59 +0100

    Add a preset-load extension implementation

--- src/plugin/ext.rs
+++ src/plugin/ext.rs
@@ -8,6 +8,7 @@ use std::ptr::NonNull;
 pub mod audio_ports;
 pub mod note_ports;
 pub mod params;
+pub mod preset_load;
 pub mod state;
 
 /// An abstraction for a CLAP plugin extension. `P` here is the plugin type. In practice, this is
---
+++ src/plugin/ext/preset_load.rs
@@ -0,0 +1,65 @@
+//! Abstractions for interacting with the `preset-load` extension.
+
+use anyhow::{Context, Result};
+use clap_sys::ext::draft::preset_load::{clap_plugin_preset_load, CLAP_EXT_PRESET_LOAD};
+use std::ffi::{CStr, CString};
+use std::ptr::NonNull;
+
+use crate::plugin::instance::Plugin;
+use crate::util::unsafe_clap_call;
+
+use super::Extension;
+
+/// Abstraction for the `preset-load` extension covering the main thread functionality.
+#[derive(Debug)]
+pub struct PresetLoad<'a> {
+    plugin: &'a Plugin<'a>,
+    preset_load: NonNull<clap_plugin_preset_load>,
+}
+
+impl<'a> Extension<&'a Plugin<'a>> for PresetLoad<'a> {
+    const EXTENSION_ID: &'static CStr = CLAP_EXT_PRESET_LOAD;
+
+    type Struct = clap_plugin_preset_load;
+
+    fn new(plugin: &'a Plugin<'a>, extension_struct: NonNull<Self::Struct>) -> Self {
+        Self {
+            plugin,
+            preset_load: extension_struct,
+        }
+    }
+}
+
+impl PresetLoad<'_> {
+    /// Try to load a preet based on a URI and an optional load keys. This information can be
+    /// obtained through the preset discovery factory
+    /// ([`Library::preset_discovery_factory()`][[crate::plugin::library::Library::preset_discovery_factory()]]).
+    /// Load keys are only used for container presets, otherwise they're `None`. The semantics are
+    /// similar to loading state.
+    pub fn from_uri(&self, uri: &str, load_key: Option<&str>) -> Result<()> {
+        let uri_cstring = CString::new(uri).context("URI contained internal null bytes")?;
+        let load_key_cstring = load_key
+            .map(|load_key| {
+                CString::new(load_key).context("Load key contained internal null bytes")
+            })
+            .transpose()?;
+
+        let preset_load = self.preset_load.as_ptr();
+        let plugin = self.plugin.as_ptr();
+        let success = unsafe_clap_call! {
+            preset_load=>from_uri(
+                plugin,
+                uri_cstring.as_ptr(),
+                load_key_cstring.map(|load_key| load_key.as_ptr()).unwrap_or(std::ptr::null())
+            )
+        };
+        if success {
+            Ok(())
+        } else {
+            anyhow::bail!(
+                "'clap_plugin_preset_load::from_uri()' returned false with URI '{uri}' and load \
+                 key '{load_key:?}'."
+            );
+        }
+    }
+}
